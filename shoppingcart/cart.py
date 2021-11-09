e05b76b59d2dec9ce506c8d2b748704d58532aa3
PHedro <pedrohenrique.reis@gmail.com>
2021-11-09 22:33:08 +0000

    create _total_entry and _receipt_entries to split the responsibility of generating the receipt and keep the functions short. the instance of the shopping cart now has a cache of the receipt and total_price of the cart. _receipt_entries resets the calculated values every time it's called to avoid duplicating the entries and return a bad receipt.

--- shoppingcart/cart.py
+++ shoppingcart/cart.py
@@ -7,24 +7,46 @@ from . import abc
 class ShoppingCart(abc.ShoppingCart):
     def __init__(self):
         self._items = OrderedDict()
+        self.receipt = []
+        self.total_price = 0.0
 
     def add_item(self, product_code: str, quantity: int):
+        # thought on changing the quantity to float to better represent
+        # items that are sold in fractions (i.e. 1.5kg of meat) keeping int
+        # to follow the abstract class
         quantity += self._items.get(product_code, 0)
         self._items.update({product_code: quantity})
 
     def print_receipt(self) -> typing.List[str]:
-        lines = []
+        self._receipt_entries()
 
+        return self.receipt
+
+    def _reset_cached_values(self):
+        self.receipt = []
+        self.total_price = 0.0
+
+    def _total_entry(self):
+        total_entry = "TOTAL - - - {total_price_string}".format(
+            total_price_string="€%.2f" % self.total_price
+        )
+        self.receipt.append(total_entry)
+        return total_entry
+
+    def _receipt_entries(self):
+        self._reset_cached_values()
         for product, quantity in self._items.items():
             price = self._get_product_price(product) * quantity
+            self.total_price += price
 
-            lines.append("{product} - {quantity} - {price_string}".format(
+            self.receipt.append("{product} - {quantity} - {price_string}".format(
                 product=product,
-                quantity=str(quantity),
+                quantity=quantity,
                 price_string="€%.2f" % price
             ))
 
-        return lines
+        self._total_entry()
+        return self.receipt, self.total_price
 
     def _get_product_price(self, product_code: str) -> float:
         price = 0.0
--- tests/test_cart.py
+++ tests/test_cart.py
@@ -12,14 +12,16 @@ class ShoppingCartTestCase(TestCase):
 
         receipt = self.cart.print_receipt()
 
-        assert receipt[0] == "apple - 1 - €1.00"
+        self.assertEqual(receipt[0], "apple - 1 - €1.00")
+        self.assertEqual(receipt[1], "TOTAL - - - €1.00")
 
     def test_add_item_with_multiple_quantity(self):
         self.cart.add_item("apple", 2)
 
         receipt = self.cart.print_receipt()
 
-        assert receipt[0] == "apple - 2 - €2.00"
+        self.assertEqual(receipt[0], "apple - 2 - €2.00")
+        self.assertEqual(receipt[1], "TOTAL - - - €2.00")
 
     def test_add_different_items(self):
         self.cart.add_item("banana", 1)
@@ -27,8 +29,9 @@ class ShoppingCartTestCase(TestCase):
 
         receipt = self.cart.print_receipt()
 
-        assert receipt[0] == "banana - 1 - €1.10"
-        assert receipt[1] == "kiwi - 1 - €3.00"
+        self.assertEqual(receipt[0], "banana - 1 - €1.10")
+        self.assertEqual(receipt[1], "kiwi - 1 - €3.00")
+        self.assertEqual(receipt[2], "TOTAL - - - €4.10")
 
     def test_receipt_ordering(self):
         self.cart.add_item("banana", 1)
@@ -38,6 +41,41 @@ class ShoppingCartTestCase(TestCase):
 
         receipt = self.cart.print_receipt()
 
-        assert receipt[0] == "banana - 2 - €2.20"
-        assert receipt[1] == "kiwi - 1 - €3.00"
-        assert receipt[2] == "apple - 1 - €1.00"
+        self.assertEqual(receipt[0], "banana - 2 - €2.20")
+        self.assertEqual(receipt[1], "kiwi - 1 - €3.00")
+        self.assertEqual(receipt[2], "apple - 1 - €1.00")
+        self.assertEqual(receipt[3], "TOTAL - - - €6.20")
+
+    def test_generate_receipt_entries(self):
+        self.cart.add_item("banana", 1)
+        self.cart.add_item("kiwi", 1)
+        self.cart.add_item("banana", 1)
+        self.cart.add_item("apple", 1)
+
+        receipt, _ = self.cart._receipt_entries()
+
+        self.assertEqual(receipt[0], "banana - 2 - €2.20")
+        self.assertEqual(receipt[1], "kiwi - 1 - €3.00")
+        self.assertEqual(receipt[2], "apple - 1 - €1.00")
+        self.assertEqual(receipt[3], "TOTAL - - - €6.20")
+
+    def test_generate_total_entry(self):
+        self.cart.add_item("banana", 1)
+        self.cart.add_item("kiwi", 1)
+        self.cart.add_item("banana", 1)
+        self.cart.add_item("apple", 1)
+        _, total_price = self.cart._receipt_entries()
+        total = self.cart._total_entry()
+
+        self.assertEqual(total, "TOTAL - - - €6.20")
+
+    def test_reset_cached_values(self):
+        self.cart.add_item("banana", 1)
+        self.cart.add_item("kiwi", 1)
+        self.cart.add_item("banana", 1)
+        self.cart.add_item("apple", 1)
+        _, total_price = self.cart._receipt_entries()
+        self.cart._reset_cached_values()
+
+        self.assertEqual(0.0, self.cart.total_price)
+        self.assertEqual(0, len(self.cart.receipt))
