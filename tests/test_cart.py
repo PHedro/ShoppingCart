05fa3c9f8f677132cfd6799fa9549da6551b8215
PHedro <pedrohenrique.reis@gmail.com>
2021-11-09 21:21:50 +0000

    migrate the functions to a test case

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,30 +1,31 @@
+from unittest import TestCase
+
 from shoppingcart.cart import ShoppingCart
 
 
-def test_add_item():
-    cart = ShoppingCart()
-    cart.add_item("apple", 1)
+class ShoppingCartTestCase(TestCase):
+    def test_add_item(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 1)
 
-    receipt = cart.print_receipt()
+        receipt = cart.print_receipt()
 
-    assert receipt[0] == "apple - 1 - €1.00"
+        assert receipt[0] == "apple - 1 - €1.00"
 
+    def test_add_item_with_multiple_quantity(self):
+        cart = ShoppingCart()
+        cart.add_item("apple", 2)
 
-def test_add_item_with_multiple_quantity():
-    cart = ShoppingCart()
-    cart.add_item("apple", 2)
+        receipt = cart.print_receipt()
 
-    receipt = cart.print_receipt()
+        assert receipt[0] == "apple - 2 - €2.00"
 
-    assert receipt[0] == "apple - 2 - €2.00"
+    def test_add_different_items(self):
+        cart = ShoppingCart()
+        cart.add_item("banana", 1)
+        cart.add_item("kiwi", 1)
 
+        receipt = cart.print_receipt()
 
-def test_add_different_items():
-    cart = ShoppingCart()
-    cart.add_item("banana", 1)
-    cart.add_item("kiwi", 1)
-
-    receipt = cart.print_receipt()
-
-    assert receipt[0] == "banana - 1 - €1.10"
-    assert receipt[1] == "kiwi - 1 - €3.00"
+        assert receipt[0] == "banana - 1 - €1.10"
+        assert receipt[1] == "kiwi - 1 - €3.00"
